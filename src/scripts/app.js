e214448e79b6763b9488b9a8218b50ab78fc9b18
Lennon Jesus <lennon.jesus@gmail.com>
2016-09-11 06:50:41 -0300

    adding bootstrap material

---
+++ src/scripts/app.js
@@ -0,0 +1,16 @@
+(() => {
+  'use strict';
+
+  var app = {
+    isLoading: true,
+    container: document.querySelector('.main'),
+    loader: document.querySelector('.loader'),
+    init: init
+  };
+
+  app.init();
+
+  function init () {
+    
+  }
+})();
